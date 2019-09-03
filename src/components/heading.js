d2254748405dda995990e2afc4a03f1b72fc4a1b
at1208 <amantiwari12081999@gmail.com>
2019-09-03 23:36:26 +0530

    initial setup

---
+++ src/components/heading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Heading = () => {
+  return <div className='container-fluid'>
+       <h1 className='text-center'>Facial Based Attendance System</h1>
+        </div>
+}
+export default Heading;
